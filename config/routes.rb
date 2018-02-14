bd1fdda5693857149452cf53062bba65acc04330
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-13 21:32:40 -0800

    Added chat rooms and messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+
+  get 'chat_rooms/show'
+
+  get 'chat_rooms/new'
+
+  get 'chat_rooms/create'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
